ecf075219bf7dc3ffe87dd526c9c06bbbe10514d
Affan <qureshiaffan99@gmail.com>
2024-06-15 22:30:55 +0530

    Music app

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Navbar'
 import { albumsData, songsData } from '../assets/assets'
 import AlbumItem from './AlbumItem'
+import SongItem from './SongItem'
 
 const DisplayHome = () => {
     return (
@@ -24,7 +25,15 @@ const DisplayHome = () => {
             <div className='mb-4'>
                 <h1 className='my-5 font-bold text-2xl'>Today's biggest Hits</h1>
                 <div className='flex overflow-auto'>
-                    
+                    {songsData.map((item, index) => (
+                        <SongItem 
+                            key={index} 
+                            name={item.name}
+                            desc={item.desc}
+                            image={item.image}
+                            id={item.id}
+                        />
+                    ))}
                 </div>
             </div>
         </>
